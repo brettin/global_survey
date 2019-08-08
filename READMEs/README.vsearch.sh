82d76e941502645bada3b55a6a3f4ea64825ad91
brettin <brettin@anl.gov>
2019-08-08 15:51:22 -0500

    change to sbatch

--- READMEs/README.vsearch.sh
+++ READMEs/README.vsearch.sh
@@ -15,22 +15,25 @@ set -e
 # files involves splitting big files so that the memory limits of the
 # compute nodes are not exceeded.
 
-BASE=/blues/gpfs/globalscratch/brettin/global_survey/danton
-export DATA=$BASE/metagenomic_data_urban
-export MCS=$BASE/metagenomic_mcs100_urban
+BASE=/blues/gpfs/globalscratch/brettin/
+# This is the directory where the fastq files live
+export DATA=$BASE/permafrost
+# This is a directormy where results and temporary files will be written
+export MCS=$BASE/permafrost_mcs
+
 cd $DATA
 
-# /home/brettin/PATRIC/brettin/global_survey/bin/split_bigfiles.sh
-# echo "parallel -j 16 {} :::: tmp.bigfiles.sh" > bigfiles.parallel
-# chmod u+x bigfiles.parallel 
-# ./bigfiles.parallel > bigfiles.log 2>&1
+/home/brettin/PATRIC/brettin/global_survey/bin/split_bigfiles.sh
+echo "parallel -j 16 {} :::: tmp.bigfiles.sh" > bigfiles.parallel
+chmod u+x bigfiles.parallel 
+./bigfiles.parallel > bigfiles.log 2>&1
 
-# /home/brettin/PATRIC/brettin/global_survey/bin/gzip_split_bigfiles.sh
-# echo "parallel -j 16 {} :::: tmp.bigfiles-gzip.sh" > bigfiles.gzip.parallel
-# chmod u+x bigfiles.gzip.parallel
-# ./bigfiles.gzip.parallel &
+/home/brettin/PATRIC/brettin/global_survey/bin/gzip_split_bigfiles.sh
+echo "parallel -j 16 {} :::: tmp.bigfiles-gzip.sh" > bigfiles.gzip.parallel
+chmod u+x bigfiles.gzip.parallel
+./bigfiles.gzip.parallel &
 
-# find . -size +8G -not -name "*split*" -exec rm {} \;
+find . -size +8G -not -name "*split*" -exec rm {} \;
 
 cd ../
 mkdir -p $MCS
@@ -46,12 +49,12 @@ split -l 40 subjects_file subjects_file
 wc subjects_file*
 rm subjects_file
 
-for n in subjects_file* ; do echo $n ; qsub -v ARG1=/lcrc/project/PATRIC/brettin/global_survey/brettin/allen/mcs.unique.60.fa,ARG2=$n /lcrc/project/PATRIC/brettin/global_survey/bin/qsub_vsearch.sh ; sleep 1 ; done > qsub.log
+
+for n in subjects_file* ; do echo $n ; sbatch --export ARG1=/lcrc/project/PATRIC/brettin/global_survey/brettin/allen/mcs.unique.60.fa,ARG2=$n /lcrc/project/PATRIC/brettin/global_survey/bin/sbatch_vsearch.sh ; sleep 1 ; done > sbatch.log
 
 # It is good to check for errors in the log files. A simple case
 # insensitive grep for error in the log files should work. Any errors
 # that are found are investigated manually and jobs are rurun if
 # necessary.
 
-# grep -i error qsub_vsearch.sh.e*
-# grep -i error qsub_vsearch.sh.o*
+grep -i error slurm-*.out
